3ce0c23969a846229cb009963919792319938cc0
RussellLVP <russell@sa-mp.nl>
2014-03-05 22:36:36 +0000

    Add a AccountData class for persistent account data
    This class roughly resembles the database structure which we'll be
    supporting, in which all the player's information will be stored between
    their playing sessions.

---
+++ src/main/java/com/mineground/account/AccountData.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2011 - 2014 Mineground, Las Venturas Playground
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mineground.account;
+
+import java.util.Date;
+
+// The AccountData class holds information about the player's profile, as it is stored in the
+// database. When adding or removing fields, please make sure they are listed in the same order as
+// they are in the actual database scheme, making this class easier to work with.
+public class AccountData {
+
+    // ***** users *********************************************************************************
+    
+    // Id of the player, which roughly corresponds to the order in which users registered.
+    public int user_id;
+    
+    // The player's username, which is not necessarily equal to their display name.
+    public String username;
+    
+    // The player's unique Id. This will be set to the Minecraft.net Id for players with an official
+    // client. For players with a cracked client, it will be an empty string.
+    public String unique_id;
+    
+    // Mineground uses PBKDF2 to store player passwords, for which the player's hashed password and
+    // salt used to hash that password are both stored in this string. The password can be validated
+    // by using Taylor Hornby's PBKDF2 password hash implementation.
+    public String password;
+    
+    // The level this player's account has. This value is not mutable from the plugin, because it
+    // would allow permanent promotions, which we'd like to limit to IRC.
+    public AccountLevel level;
+    
+    // The date on which the player first joined Mineground. This value is not mutable from the
+    // plugin, because it doesn't make sense to change the registration date.
+    public Date registered;
+    
+    // ***** users_settings ************************************************************************
+    
+    // The total number of seconds the player has spent online on Mineground. This does include
+    // time during which the player was idle.
+    public int online_time;
+    
+    // The number of living entities, including both players, monsters and other NPCs, the player
+    // has killed during their playing on the server.
+    public int kill_count;
+    
+    // The number of times the player has died, either by being killed or from other causes.
+    public int death_count;
+    
+    // The number of reaction tests this player has won.
+    public int stats_reaction;
+    
+    // The number of blocks this player has created, in all worlds and gamemodes.
+    public int stats_blocks_created;
+    
+    // The number of blocks this player has destroyed, in all worlds and gamemodes.
+    public int stats_blocks_destroyed;
+    
+    // The date at which the player last connected to the server.
+    public Date last_seen;
+}
---
+++ src/main/java/com/mineground/account/AccountLevel.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2011 - 2014 Mineground, Las Venturas Playground
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mineground.account;
+
+// This class contains enumerations for the account levels supported by Mineground. These must match
+// the "level" enumeration in the "users" table in the database.
+public enum AccountLevel {
+    // Guests don't have registered their account on the website, and therefore have very limited
+    // rights on the server. They can talk and use a limited amount of commands, but are not allowed
+    // to build anywhere on the server.
+    Guest,
+    
+    // Builders have registered their account on the website, and have validated their e-mail
+    // address. They have the ability to build outside protected areas on Mineground.
+    Builder,
+    
+    // Super Builders are experienced players who have demonstrated to be both responsible and
+    // knowledgeable in their playing on Mineground. They have access to the /fly command.
+    SBuilder,
+    
+    // VIPs have donated to the Mineground community, and therefore have a special status. They
+    // receive the same benefits as the SBuilder class.
+    VIP,
+    
+    // TODO: Describe this accurately.
+    Moderator,
+    
+    // TODO: Describe this accurately.
+    Administrator,
+    
+    // Management members have all possible rights on the server, and will be made server operators
+    // with the Bukkit server as well, automagically giving them admin rights for all other plugins.
+    Management;
+    
+    // Returns the AccountLevel corresponding to the string |level|, per our database conventions.
+    public static AccountLevel fromString(String level) {
+        switch (level) {
+            case "Builder":
+                return Builder;
+            case "SBuilder":
+                return SBuilder;
+            case "VIP":
+                return VIP;
+            case "Moderator":
+                return Moderator;
+            case "Administrator":
+                return Administrator;
+            case "Management":
+                return Management;
+        }
+
+        return Guest;
+    }
+    
+    // Returns the String corresponding to the AccountLevel |level|.
+    public static String toString(AccountLevel level) {
+        switch (level) {
+            case Builder:
+                return "Builder";
+            case SBuilder:
+                return "SBuilder";
+            case VIP:
+                return "VIP";
+            case Moderator:
+                return "Moderator";
+            case Administrator:
+                return "Administrator";
+            case Management:
+                return "Management";
+        }
+        
+        return "Guest";
+    }
+}
