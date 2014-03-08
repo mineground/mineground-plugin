42f58fe58149767c992a94c48cefbd135a05a736
RussellLVP <russell@sa-mp.nl>
2014-03-08 23:46:54 +0000

    Add Color and TextDecoration classes
    These provide convenient mappings between Minecraft's codes (prefixed
    with a section character) and a constant for use in code.

---
+++ src/main/java/com/mineground/base/Color.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2011 - 2014 Mineground, Las Venturas Playground
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mineground.base;
+
+// The color class contains the colors available for formatting in Minecraft's chat. These colors
+// may be used for both the in-game chat and console chat.
+public class Color {
+    public static final String BLACK = "§0";
+    public static final String DARK_BLUE = "§1";
+    public static final String DARK_GREEN = "§2";
+    public static final String DARK_AQUA = "§3";
+    public static final String DARK_RED = "§4";
+    public static final String DARK_PURPLE = "§5";
+    public static final String GOLD = "§6";
+    public static final String GRAY = "§7";
+    public static final String DARK_GRAY = "§8";
+    public static final String BLUE = "§9";
+    public static final String GREEN = "§a";
+    public static final String AQUA = "§b";
+    public static final String RED = "§c";
+    public static final String LIGHT_PURPLE = "§d";
+    public static final String YELLOW = "§e";
+    public static final String WHITE = "§f";
+}
---
+++ src/main/java/com/mineground/base/TextDecoration.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2011 - 2014 Mineground, Las Venturas Playground
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mineground.base;
+
+// The following text decoration styles are available in Minecraft. They may be used at any point in
+// messages intended for either a player, or for the console.
+public class TextDecoration {
+    public static final String OBFUSCATED = "§k";
+    public static final String BOLD = "§l";
+    public static final String STRIKETHROUGH = "§m";
+    public static final String UNDERLINE = "§n";
+    public static final String ITALIC = "§o";
+    public static final String RESET = "§r";
+}
